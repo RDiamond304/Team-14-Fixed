8204eb8c55a37db45b4928f4dd85423e63f404dc
jonathancheung <j_cheung@utexas.edu>
2016-04-23 22:22:31 -0500

    Added Data to Company and Industry (No FK)

--- System_Project_Group14/Migrations/Configuration.cs
+++ System_Project_Group14/Migrations/Configuration.cs
@@ -19,7 +19,7 @@ namespace System_Project_Group14.Migrations
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data. E.g.
-            //
+            
             //    context.People.AddOrUpdate(
             //      p => p.FullName,
             //      new Person { FullName = "Andrew Peters" },
